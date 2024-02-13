276bcb1f79a4707a53471db65e7ce10e75bc032c
scarlettllc <liangchuanluo@gmail.com>
2024-02-13 21:17:54 +0800

    2024.02.13 answer_reasoning/predict_nsm.sh

--- answer_reasoning/predict_nsm.sh
+++ answer_reasoning/predict_nsm.sh
@@ -8,4 +8,4 @@ then
   num_epoch=30
 fi
 
-CUDA_VISIBLE_DEVICES=3 python main_nsm.py --name ${ds_tag} --model_name gnn --data_folder ./datasets/${ds_tag}_EPR/ --checkpoint_dir checkpoint/pretrain/ --batch_size 20 --test_batch_size 40 --num_step ${num_step} --entity_dim 50 --word_dim 300 --kg_dim 100 --kge_dim 100 --eval_every 2 --experiment_name ${ds_tag}_nsm --eps 0.95 --num_epoch ${num_epoch} --use_self_loop --lr 5e-4 --q_type seq --word_emb_file word_emb_300d.npy --reason_kb --encode_type --loss_type kl --is_eval --load_experiment ${ds_tag}_nsm-f1.ckpt
+CUDA_VISIBLE_DEVICES=0 python main_nsm.py --name ${ds_tag} --model_name gnn --data_folder ./datasets/${ds_tag}_EPR/ --checkpoint_dir checkpoint/pretrain/ --batch_size 20 --test_batch_size 40 --num_step ${num_step} --entity_dim 50 --word_dim 300 --kg_dim 100 --kge_dim 100 --eval_every 2 --experiment_name ${ds_tag}_nsm --eps 0.95 --num_epoch ${num_epoch} --use_self_loop --lr 5e-4 --q_type seq --word_emb_file word_emb_300d.npy --reason_kb --encode_type --loss_type kl --is_eval --load_experiment ${ds_tag}_nsm-f1.ckpt
